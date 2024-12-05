109bbaf5abd940ff9a94ba50259cc83508bdfdcd
TrulyNotMalware <notleebutyee@gmail.com>
2024-12-05 15:57:39 +0900

    fix : shouldContainAll test

--- infrastructure/src/test/kotlin/app/discord/user/UserRepositoryTest.kt
+++ infrastructure/src/test/kotlin/app/discord/user/UserRepositoryTest.kt
@@ -31,6 +31,7 @@ class UserRepositoryTest @Autowired constructor(
             then("successfully insert"){
                 insertUser.userIdentifier shouldBe validUser.userIdentifier
                 (insertUser.registerTime isSame validUser.registerTime) shouldBe true
+                insertUser.getTotalAttendanceHistories() shouldContainAll validUser.getTotalAttendanceHistories()
             }
         }
     }
